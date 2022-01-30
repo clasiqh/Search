d95a7669b639975a8273e6cd8b39c35930567ba3
clasiqh <shivam.chauhan126@gmail.com>
2022-01-30 20:07:00 +0530

    port fix

--- server.js
+++ server.js
@@ -2,21 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT | 8080;
 
 app.listen(PORT, () => {
   console.log("server started.");
 });
 
 app.use(express.static("public"));
-
-/*app.get("/", (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-
-  fs.readFile("./index.html", (err, data) => {
-    res.write(data);
-    res.end();
-  });
-});*/
