74f30d2f5a261a9078aebbe87538fa92288e6df3
clasiqh <shivam.chauhan126@gmail.com>
2022-01-30 20:18:31 +0530

    port fix, again.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-const PORT = process.env.PORT | 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("server started.");
