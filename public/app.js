0d57f4249db771d0ce868fce314806999b64528f
clasiqh <shivam.chauhan126@gmail.com>
2022-01-30 19:32:42 +0530

    animations fix

--- public/app.js
+++ public/app.js
@@ -7,13 +7,22 @@ input.addEventListener("keyup", (e) => {
   if (text == "") {
     suggestions.style.display = "none";
   } else {
-    suggestions.style.display = "block";
-
     for (var i = 0; i < 4; i++) {
       let div = document.createElement("div");
       div.innerHTML = text;
       div.className = "box";
       suggestions.appendChild(div);
     }
+    suggestions.style.display = "flex";
+    suggestions.style.width = "500px";
+    suggestions.style.opacity = 100;
   }
+});
+
+document.body.addEventListener("click", (e) => {
+  suggestions.style.opacity = 0;
+  suggestions.style.width = "360px";
+  setTimeout(() => {
+    suggestions.style.display = "none";
+  }, 200);
 });
