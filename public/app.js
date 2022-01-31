fb8dbc44a3c882234c7b614dd5b37af7e7659450
clasiqh <shivam.chauhan126@gmail.com>
2022-01-31 23:40:47 +0530

    brute force words

--- public/app.js
+++ public/app.js
@@ -1,28 +1,66 @@
 const input = document.getElementById("query");
 const suggestions = document.getElementById("suggestions");
+document.body.addEventListener("click", closeSuggestions);
 
-input.addEventListener("keyup", (e) => {
-  suggestions.innerHTML = "";
+let state = false;
+const numberOfSuggestions = 5;
+
+input.addEventListener("keyup", async (event) => {
+  suggestions.innerHTML = ""; //remove previous elements
   const text = input.value;
+
+  //if empty input
   if (text == "") {
-    suggestions.style.display = "none";
-  } else {
-    for (var i = 0; i < 4; i++) {
-      let div = document.createElement("div");
-      div.innerHTML = text;
-      div.className = "box";
-      suggestions.appendChild(div);
-    }
+    closeSuggestions();
+    return;
+  }
+  let index = words.indexOf(text);
+
+  //if word exists, print it and show suggestions
+  if (index != -1) {
+    for (let i = index; i < index + numberOfSuggestions && i < length; i++)
+      addBox(words[i]);
+    showSuggestions();
+    return;
+  }
+
+  //if words doesn't exist
+  closeSuggestions();
+});
+
+function search() {
+  const text = input.value;
+  window.open("https://www.google.com/search?q=" + text);
+}
+
+function closeSuggestions() {
+  if (state) {
+    suggestions.style.opacity = 0;
+    suggestions.style.width = "360px";
+    setTimeout(() => {
+      suggestions.style.display = "none";
+    }, 200);
+    state = false;
+  }
+}
+
+function showSuggestions() {
+  if (!state) {
     suggestions.style.display = "flex";
     suggestions.style.width = "500px";
     suggestions.style.opacity = 100;
+    state = true;
   }
-});
+}
 
-document.body.addEventListener("click", (e) => {
-  suggestions.style.opacity = 0;
-  suggestions.style.width = "360px";
-  setTimeout(() => {
-    suggestions.style.display = "none";
-  }, 200);
-});
+const addBox = (text) => {
+  let div = document.createElement("div");
+  div.innerHTML = text;
+  div.className = "box";
+  div.addEventListener("click", () => {
+    input.value = text;
+  });
+  suggestions.appendChild(div);
+};
+
+const length = words.length;
